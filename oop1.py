7d3e7985072dd25e6088666ede6b897be0697ffb
Emmanuel Rotich <71790505+mikemanuu@users.noreply.github.com>
2024-11-04 12:54:46 +0300

    commit

---
+++ oop1.py
@@ -0,0 +1,20 @@
+
+class Student:
+    #PROPERTIES/VARIABLES/ATTRIBUTES/CHARACTERISTICS
+    name = "Evans"
+    gender = "Male"
+    age = 32
+
+    #Behaviours/Method/Function
+    def study(self):
+        print("Student is studying")
+
+#object1
+student1 = Student()     #creating an object
+student1.study()
+print(student1.name)
+
+#object2
+student2 = Student()
+
+
